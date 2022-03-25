7838839ac8f9b06639b0b62a03d32a21732e5c87
TDCIAN <tdcian71@gmail.com>
2022-03-25 17:22:43 +0900

    위장

--- CodingTestKit/MyPlayground.playground/Contents.swift
+++ CodingTestKit/MyPlayground.playground/Contents.swift
@@ -220,3 +220,96 @@ let completion3: [String] = ["stanko", "ana", "mislav"]
 //print("첫 번째: \(solution(phoneBook: case1))")
 //print("두 번째: \(solution(phoneBook: case2))")
 //print("세 번째: \(solution(phoneBook: case3))")
+
+// MARK: 해시 - 3 / 위장(https://programmers.co.kr/learn/courses/30/lessons/42578)
+/*
+ 1) 문제 설명
+ 스파이들은 매일 다른 옷을 조합하여 입어 자신을 위장합니다.
+ 예를 들어 스파이가 가진 옷이 아래와 같고 오늘 스파이가 동그란 안경, 긴 코트, 파란색 티셔츠를 입었다면
+ 다음날은 청바지를 추가로 입거나 동그란 안경 대신 검정 선글라스를 착용하거나 해야 합니다.
+ 
+ 종류 / 이름
+ 얼굴 / 동그란 안경, 검정 선글라스
+ 상의 / 파란색 티셔츠
+ 하의 / 청바지
+ 겉옷 / 긴 코트
+ 
+ 스파이가 가진 의상들이 담긴 2차원 배열 clothes가 주어질 때 서로 다른 옷의 조합의 수를 return하도록
+ solution 함수를 작성해주세요.
+ 
+ 2) 제한사항
+ - clothes의 각 행은 [의상의 이름, 의상의 종류]로 이루어져 있습니다.
+ - 스파이가 가진 의상의 수는 1개 이상 30개 이하입니다.
+ - 같은 이름을 가진 의상은 존재하지 않습니다.
+ - clothes의 모든 원소는 문자열로 이루어져 있습니다.
+ - 모든 문자열의 길이는 1 이상 20 이하인 자연수이고 알파벳 소문자 또는 '_'로만 이루어져 있습니다.
+ - 스파이는 하루에 최소 한 개의 의상은 입습니다.
+ 
+ 3) 입출력 예
+ [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]] -> return 5
+ [["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]] -> return 3
+
+ 
+ 4) 입출력 예 설명
+ 예제 #1
+ - headgear에 해당하는 의상이 yellow_hat, green_turban이고 eyewear에 해당하는 의상이 blue_sunglasses이므로
+   아래와 같이 5개의 조합이 가능합니다.
+ 1. yellow_hat
+ 2. blue_sunglasses
+ 3. green_turban
+ 4. yellow_hat + blue_sunglasses
+ 5. green_turban + blue_sunglasses
+ 
+ 예제 #2
+ - face에 해당하는 의상이 crow_mask, blue_sunglasses, smoky_makeup이므로 아래와 같이 3개의 조합이 가능합니다.
+ 1. crow_mask
+ 2. blue_sunglasses
+ 3. smoky_makeup
+ */
+
+/*
+ 파이썬3 풀이 참조 -> https://coding-grandpa.tistory.com/88
+ 지금 나온건 프로그래머스 사이트에 있는 다른 풀이
+ def solution(clothes):
+    clothes_type = {}
+ 
+    for cloth, type in clothes:
+        if type not in clothes_type:
+            clothes_type[type] = 2
+        else:
+            clothes_type[type] += 1
+ 
+    answer = 1
+    for number in clothes_type.values():
+        answer *= number
+ 
+    return answer - 1
+ */
+
+//let case1 = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+//let case2 = [["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
+//
+//// Swift 풀이
+//func solution(clothes: [[String]]) -> Int {
+//
+//    var clothesType: [String: Int] = [:]
+//    for cloth in clothes {
+//        let type = cloth[1]
+//        if !clothesType.keys.contains(type) {
+//            clothesType[type] = 2
+//        } else {
+//            clothesType[type]! += 1
+//        }
+//    }
+//
+//    var answer: Int = 1
+//    for number in clothesType.values {
+//        answer *= number
+//    }
+//
+//    return answer - 1
+//}
+//
+//print("1번 케이스: \(solution(clothes: case1))")
+//print("2번 케이스: \(solution(clothes: case2))")
+
